146232afeea46fb22cbff55d13eaad925db00b3c
anaccteles <anacarlacortes01@gmail.com>
2025-02-15 12:02:10 -0300

    feat: add typescript + tailwindcss

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <article className="text-nowrap text-amber-400">
+      <h3>Beloved Manhattan soup stand closes</h3>
+      <p>New Yorkers are facing the winter chill...</p>
+    </article>
+  );
+}
+
+export default App;
