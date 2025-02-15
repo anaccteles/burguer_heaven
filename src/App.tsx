1c8430ef74406a31646dc33d5cf90798b8bf0219
anaccteles <anacarlacortes01@gmail.com>
2025-02-15 13:55:49 -0300

    add section detach

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { SectionDetach } from './SectionDetach';
 
 function App() {
   return (
-    <div className="bg-[#1E1E1E] w-full  h-[100vh]">
+    <div>
       <NavBar />
       <SectionDetach />
     </div>
--- src/SectionDetach.tsx
+++ src/SectionDetach.tsx
@@ -1,3 +1,11 @@
+import burgers from './assets/burgers.png';
 export function SectionDetach() {
-  return <div />;
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <h1 className="text-[#E9E3DC] text-center max-w-[1255px] text-[107.09px] leading-none">
+        NEW YORK’S FAVORITE ORGANIC HAMBURGER JOINT
+      </h1>
+      <img src={burgers} alt="Burger" className="h-[478px] mt-[-45px]" />
+    </div>
+  );
 }
