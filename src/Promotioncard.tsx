3a24005cdfd9be1ef67834922adda839bd7c0b13
anaccteles <anacarlacortes01@gmail.com>
2025-03-05 19:32:10 -0300

    atualizando a seção Promotioncard

--- src/Promotioncard.tsx
+++ src/Promotioncard.tsx
@@ -1,7 +1,14 @@
 export function Promotioncard() {
   return (
-    <div>
-      <h1>Olá Mundo!</h1>
+    <div className="bg-[#D1EF53] mr-12 ml-4 p-11 rounded-[15px]">
+      <h1 className="text-[#1E1E1E] text-[70px] w-[50%] leading-15">
+        THE BURGUER ABOVE ALL BURGES
+      </h1>
+      <h1 className="text-[#1E1E1E]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec
+        ornare neque.
+      </h1>
+      <button>ABOUT US</button>
     </div>
   );
 }
