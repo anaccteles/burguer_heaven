b8625245aba123d3ba1707a1deee9c5389189d0c
anaccteles <anacarlacortes01@gmail.com>
2025-04-29 19:35:36 -0300

    Atualização do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,19 @@ function App() {
     <div>
       <NavBar />
       <div className="flex flex-col items-center">
-        <div className="container">
+        <div className="pr-12 pl-12">
           <SectionDetach />
           <Promotioncard />
-          <div className="flex">
+          <div className="flex justify-between">
             <Cardorder text="SALT & VINEGAR FRENCH FRIES" img={batata} />
             <Cardorder text="CRISPY CHICKEN SANDWICH" img={hamburguers} />
           </div>
+          <h1 className="text-[#D1EF53] text-center max-w-[1255px] text-[107.09px] leading-none mt-12">
+            "THE BEST BURGUER I'VE EVER HAD"
+          </h1>
+          <h1 className="text-[#E9E3DC] text-center max-w-[1255px] text-[70px] leading-none mt-12">
+            ORDER ONLINE OR COME VISIT US TODAY
+          </h1>
         </div>
       </div>
     </div>
